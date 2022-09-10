bf49652217d5816cb8a8bc866a76722b519321f1
Kazem Faghih <gkazemf78@gmail.com>
2022-09-10 16:34:42 +0430

    Modify benchmark script for more configurability and generalization Add a simple script to verify that results of different methods match

--- timeit.sh
+++ timeit.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# ARGUMENTS for this script: 1. targets_file, 2. query_file, 3. short_sequence_guarantee! (input files should have sequences in each line and not be fasta formatted!)
+
 f () {
-mynvcc="nvcc -arch=sm_50"
-files="LocalGaplessAlignmentGPU.cu Utils.cpp ScoreMatrix.cpp"
-files2="LocalGaplessAlignmentGPUWarpShuffles.cu Utils.cpp ScoreMatrix.cpp"
+nvcc_flags="-arch=sm_50"
+# these are for the cases that target_arch is NOT "armv7l aarch64 sbsa" [arch can be known with "uname -m"] [35 & 37 are excluded from the list for now because of deprecation!]
+general_arch_nums="50 52 60 61 70 75 80 86"
+for num in $general_arch_nums; do nvcc_flags_general+="-gencode arch=compute_$num,code=sm_$num "; done
+mynvcc="nvcc $nvcc_flags_general"
+sup_files="Utils.cpp ScoreMatrix.cpp"
+cpp_main_file="LocalGaplessAlignmentCPU.cpp"
+cu_main_file="LocalGaplessAlignmentGPUWarpShuffles.cu"
+# files="LocalGaplessAlignmentGPU.cu Utils.cpp ScoreMatrix.cpp"
 flags="-lineinfo"
 obj_file="exe.out"
 out_pipe="tail -5 | sed '2d'"
 # exe_args="TestSamples/bulk_tiny.txt 0 0 TestSamples/bulk_query.txt"
-targets="TestSamples/bulk_tiny2.txt"
-query="TestSamples/bulk_query.txt"
+targets="TestSamples/targets.txt"
+query="TestSamples/queries.txt"
 if [ ! -z "$1" ]
   then
     targets="$1"
@@ -20,35 +28,40 @@ if [ ! -z "$1" ]
 fi
 
 
-base_command="$mynvcc $files $flags $default_defines -o $obj_file && ./$obj_file $exe_args | $out_pipe"
+base_command="$mynvcc $cu_main_file $sup_files $flags {sup_flags} -o $obj_file && ./$obj_file {<target_file> <is_fasta> <IS on_columns> <query_file>} | {filter output}"
+echo $base_command
 
 echo "Time: $(date +"%Y-%m-%d_%H-%M-%S")"
-echo "#### ON_DIAGONAL method without REDUCTION (with ATOMIC_FUNCTION)"
-command1="$mynvcc $files $flags -o $obj_file && ./$obj_file $targets 0 0 $query | $out_pipe"
+sup_flags=("-DHANDLE_LONG_SEQUENCE")
+if [ ! -z "$3" ] && [ "$3" = "0" ]; then sup_flags=("" "-DHANDLE_LONG_SEQUENCE"); fi
+
+for sup_flag in "${sup_flags[@]}"
+do
+if [ ! -z $sup_flag ]; then echo -e "\n**** The current ADDITIONAL FLAG is: $sup_flag ****"; fi
+echo -e "\n#### ON_DIAGONAL method without REDUCTION (with ATOMIC_FUNCTION)"
+command1="$mynvcc $cu_main_file $sup_files $flags $sup_flag -o $obj_file && ./$obj_file $targets 0 0 $query | $out_pipe"
 eval $command1 #>> $outfilename
 
 echo -e "\n#### ON_DIAGONAL method with REDUCTION"
-command2_1="$mynvcc $files $flags -DREDUCE_ALIGNMENT_RESULT -o $obj_file && ./$obj_file $targets 0 0 $query | $out_pipe"
-eval $command2_1 #>> $outfilename
-
-echo -e "\n#### ON_DIAGONAL method with MORE efficient REDUCTION and handling sequences longer than 1024"
-command2_2="$mynvcc $files2 $flags -DREDUCE_ALIGNMENT_RESULT -o $obj_file && ./$obj_file $targets 0 0 $query | $out_pipe"
-eval $command2_2 #>> $outfilename
-
+command2="$mynvcc $cu_main_file $sup_files $flags $sup_flag -DREDUCE_ALIGNMENT_RESULT -o $obj_file && ./$obj_file $targets 0 0 $query | $out_pipe"
+eval $command2 #>> $outfilename
 
 echo -e "\n#### ON_COLUMNS method without REDUCTION (with ATOMIC_FUNCTION)"
-command3="$mynvcc $files $flags -o $obj_file && ./$obj_file $targets 0 1 $query | $out_pipe"
+command3="$mynvcc $cu_main_file $sup_files $flags $sup_flag -o $obj_file && ./$obj_file $targets 0 1 $query | $out_pipe"
 eval $command3 #>> $outfilename
 
-echo -e "\n#### ON_COLUMNS method with REDUCTION (reduce on COLUMNS)"
-command4="$mynvcc $files $flags -DREDUCE_ON_COLUMNS -DREDUCE_ALIGNMENT_RESULT -o $obj_file && ./$obj_file $targets 0 1 $query | $out_pipe"
+echo -e "\n#### ON_COLUMNS method with REDUCTION"
+command4="$mynvcc $cu_main_file $sup_files $flags $sup_flag -DREDUCE_ALIGNMENT_RESULT -o $obj_file && ./$obj_file $targets 0 1 $query | $out_pipe"
 eval $command4 #>> $outfilename
-
-# echo -e "\n#### ON_COLUMNS method with REDUCTION (reduce on DIAGONALS)"
-# command5="$mynvcc $files $flags -DREDUCE_ALIGNMENT_RESULT -o $obj_file && ./$obj_file $targets 0 1 $query | $out_pipe"
-# eval $command5 #>> $outfilename
+done
 }
 
+# NOTE: the last component of the path can't be a symlink and NO "cd" should have happened before this!
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 outfilename="PlayGround/benchmark_$(date +"%Y-%m-%d_%H-%M-%S")"
+start=$(date +%s.%N)
 f "$@" | tee $outfilename
-
+end=$(date +%s.%N)
+runtime=$( echo "$end - $start" | bc -l )
+printf "%.3f seconds was taken!\n" $runtime | tee -a $outfilename
---
+++ verify.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# ARGUMENTS for this script: 1. main_cuda_file, 2. targets_file, 3. query_file (input files should have sequences in each line and not be fasta formatted!)
+mynvcc="nvcc -arch=sm_50"
+sup_files="Utils.cpp ScoreMatrix.cpp"
+cpp_main_file="LocalGaplessAlignmentCPU.cpp"
+cu_main_file="LocalGaplessAlignmentGPUWarpShuffles.cu"
+flags="-lineinfo"
+obj_file="exe.out"
+# exe_args="TestSamples/bulk_tiny.txt 0 0 TestSamples/bulk_query.txt"
+targets="TestSamples/targets.txt"
+query="TestSamples/queries.txt"
+if [ ! -z "$1" ]
+then
+    cu_main_file="$1"
+    if [ ! -z "$2" ]
+    then
+        targets="$2"
+        if [ ! -z "$3" ]
+        then
+            query="$3"
+        fi
+    fi
+fi
+
+
+tmp_file1="res1.tmp"
+tmp_file2="res2.tmp"
+tmp_file3="res3.tmp"
+
+# echo "Time: $(date +"%Y-%m-%d_%H-%M-%S")"
+start=$(date +%s.%N)
+#### capturing ON_DIAGONAL and ON_COLUMNS methods with REDUCTION outputs
+flags="$flags -DDEBUG -DREDUCE_ALIGNMENT_RESULT"
+command1="$mynvcc $cu_main_file $sup_files $flags -o $obj_file && ./$obj_file $targets 0 0 $query"
+command2="$mynvcc $cu_main_file $sup_files $flags -o $obj_file && ./$obj_file $targets 0 1 $query"
+eval $command1 > $tmp_file1
+eval $command2 > $tmp_file2
+
+flags="-DSHOW_RESULTS"
+command3="g++ $cpp_main_file $sup_files $flags -o $obj_file && ./$obj_file $targets 0 $query"
+eval $command3 > $tmp_file3
+
+diff13=$(diff $tmp_file3 <(tail -n +3 $tmp_file1) | wc -l)
+diff23=$(diff $tmp_file3 <(tail -n +3 $tmp_file2) | wc -l)
+end=$(date +%s.%N)
+runtime=$( echo "$end - $start" | bc -l )
+printf "%.3f seconds was taken!\n" $runtime
+if [ $diff13 -eq 0 ] && [ $diff23 -eq 0 ]
+then
+    echo "The results match and it seems OK :D"
+    # diff $tmp_file{1,2} | head
+    head $tmp_file{1,2} -n5
+else
+    echo "Something's wrong :(("
+    diff $tmp_file3 <(tail -n +3 $tmp_file1) | head
+    diff $tmp_file3 <(tail -n +3 $tmp_file2) | head
+fi
+
+rm $tmp_file{1,2,3}
